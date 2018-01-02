82a17f2c43d2586798b233777b4ae03d247001ee
Abhishek Bisht <arkk.abhi1@gmail.com>
2018-01-02 20:38:35 +0530

    modfied QuestionsFetcher to make the last question same for every participant

--- app/src/main/java/in/leaf/abhi/treasurehunt/QuestionsFetcher.java
+++ app/src/main/java/in/leaf/abhi/treasurehunt/QuestionsFetcher.java
@@ -10,15 +10,21 @@ public class QuestionsFetcher extends AsyncTask<Object,Void,Question[]> {
     public Question[] doInBackground(Object... args) {
         Database db=(Database)args[0];
         int qSet[]=(int[])args[1];
-        int availableQuestions=qSet.length;
-        Question[] questions=new Question[availableQuestions];
+        int totalQuestions=(Integer)args[2];
+        Question[] questions=new Question[qSet.length];
+        int randomQuestions=qSet.length-1;// length-1 because the last question will be fixed
         QuestionDao qD=db.getQuestionDao();
-        for(int i=0;i<availableQuestions;i++) {
+        for(int i=0;i<randomQuestions;i++) {
             try {
                 questions[i] = qD.getQuestion(qSet[i]);
             }catch(Exception e) {
                 System.out.println("Exception occured while fetching question in Asynctask"+qSet[i]);
             }
+        }
+        try {
+            questions[randomQuestions]=qD.getQuestion(totalQuestions);
+        }catch(Exception e) {
+            System.out.println("Exception occured while fetching last question");
         }
         return questions;
     }
