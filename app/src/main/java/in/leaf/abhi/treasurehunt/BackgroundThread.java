4cf89f023de45eca221961d9c662d8d8adad8c39
Abhishek Bisht <arkk.abhi1@gmail.com>
2018-01-04 16:33:36 +0530

    Added a new class representing a background thread. This class can be used to run background taks such as saving data in database.

---
+++ app/src/main/java/in/leaf/abhi/treasurehunt/BackgroundThread.java
@@ -0,0 +1,60 @@
+package in.leaf.abhi.treasurehunt;
+
+/**
+ * Created by 500060150 on 04-01-2018.
+ */
+
+public class BackgroundThread implements Runnable {
+    private Runnable task;
+    private boolean suspended;
+    private boolean alive;
+
+    public BackgroundThread(Runnable taks) {
+        this.task=taks;
+        new Thread(this).start();
+    }
+
+    synchronized public void stop() {
+        alive=false;
+    }
+
+    private void suspend() {
+        suspended=true;
+    }
+
+    synchronized private void resume() {
+        suspended=false;
+        notify();
+    }
+
+    synchronized public void execute(Runnable task) {
+        while(task!=null) {
+            try {
+                wait();
+            }catch (InterruptedException iE) {
+                iE.printStackTrace();
+            }
+        }
+        this.task=task;
+        resume();
+    }
+
+    @Override
+    synchronized public void run() {
+        alive=true;
+        while(alive) {
+            while(suspended) {
+                try {
+                    wait();
+                }catch(InterruptedException iE) {
+                    iE.printStackTrace();
+                }
+            }
+            if(task!=null)
+                task.run();
+            task=null;
+            System.out.println("Task Completed\n\n\n\n");
+            suspend();
+        }
+    }
+}
