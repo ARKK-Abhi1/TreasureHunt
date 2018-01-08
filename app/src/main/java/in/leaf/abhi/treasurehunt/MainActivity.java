be39db6ff745abb706f537d137912dd2cd1c995f
Abhishek Bisht <arkk.abhi1@gmail.com>
2018-01-08 23:00:20 +0530

    Added a new functionality The back button is disabled in the questions activity and a person cannot log in twice from a mobile

--- app/src/main/java/in/leaf/abhi/treasurehunt/MainActivity.java
+++ app/src/main/java/in/leaf/abhi/treasurehunt/MainActivity.java
@@ -1,22 +1,48 @@
 package in.leaf.abhi.treasurehunt;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.content.Intent;
+import in.leaf.abhi.treasurehunt.database.*;
 
 public class MainActivity extends AppCompatActivity {
     private final long SPLASH_DISPLAY_TIME=2000;
+    Results results;
+    long timeTaken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        BackgroundThread bgt=BackgroundThread.getBackgroundThread(true);
+        bgt.execute(new Runnable() {
+            @Override
+            public void run() {
+                Database db= Room.databaseBuilder(MainActivity.this,Database.class,"AppDatabase").build();
+                ResultsDao rD=db.getResultsDao();
+                results=rD.getResults();
+                if(results==null)
+                    timeTaken=-1;
+                else
+                    timeTaken=results.timeInMillis;
+            }
+        });
+        while(timeTaken==0);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent logAct=new Intent(MainActivity.this,LoginActivity.class);
-                MainActivity.this.startActivity(logAct);
+                if(timeTaken==-1) {
+                    Intent logAct = new Intent(MainActivity.this, LoginActivity.class);
+                    MainActivity.this.startActivity(logAct);
+                }
+                else {
+                    Intent intent=new Intent(MainActivity.this,Results_Activity.class);
+                    intent.putExtra("timeTaken",timeTaken);
+                    intent.putExtra("questionsCompleted",results.questionsCompleted);
+                    intent.putExtra("availableQuestions",results.availableQuestions);
+                    MainActivity.this.startActivity(intent);
+                }
                 MainActivity.this.finish();
             }
         },SPLASH_DISPLAY_TIME);
